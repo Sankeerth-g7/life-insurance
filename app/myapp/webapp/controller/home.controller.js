97fe95d5a72b946b5022f8fbbb6d08ae2d96bda8
Tharuni Naini <tharuninaini@gmail.com>
2025-05-16 10:57:30 +0000

    homepage update

--- app/myapp/webapp/controller/home.controller.js
+++ app/myapp/webapp/controller/home.controller.js
@@ -6,6 +6,35 @@ sap.ui.define([
 
     return Controller.extend("myapp.controller.home", {
      onInit: function () {
-     }
+        var oCarousel = this.byId("imageCarousel");
+        var iCurrentPage = 0;
+        
+        setInterval(function () {
+            var aPages = oCarousel.getPages();
+            iCurrentPage = (iCurrentPage + 1) % aPages.length;
+            oCarousel.setActivePage(aPages[iCurrentPage]);
+            }, 3000); // 3000 ms = 3 seconds
+        },
+        
+        
+    onNavHome: function () {
+        this.getOwnerComponent().getRouter().navTo("Home");
+    },
+     onNavViewPolicy: function () {
+        this.getOwnerComponent().getRouter().navTo("ViewPolicy");
+    },
+    onNavMyProfile: function () {
+        this.getOwnerComponent().getRouter().navTo("MyProfile");
+    },
+    onNavMyPolicy: function () {
+        this.getOwnerComponent().getRouter().navTo("MyPolicy");
+    },
+    onLogout: function () {
+        MessageToast.show("You have been logged out.");
+        // Optionally navigate to login or home page
+        this.getOwnerComponent().getRouter().navTo("Login");
+    }
     });
 });
+
+
